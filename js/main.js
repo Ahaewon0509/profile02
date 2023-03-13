a1798c4b57b8a45a4691ee27c0c3b229284e3d93
Ahaewon0509 <cjstkthrkwhrgodqhrgo@naver.com>
2023-03-14 02:09:38 +0900

    v.1-3

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+/*스크롤 풀 페이지*/
+$(function() {
+    const $slider = $('.slider');
+    
+    $slider.slick({
+        infinite: false,
+        arrows: false,
+        vertical: true,
+        dots: true,
+        speed: 1200,
+        cssEase: 'cubic-bezier(0.86, 0, 0.07, 1)'
+});
+
+    $slider.mousewheel(function(e) {
+        e.preventDefault()
+        if (e.deltaY < 0) {
+
+            if ($(this).slick('slickCurrentSlide') === $(this).find('.slick-slide').length - 1) {
+            return
+            }
+
+            $(this).slick('slickNext');
+
+        } else {
+
+            if ($(this).slick('slickCurrentSlide') === 0) {
+            return
+            }
+
+            $(this).slick('slickPrev')
+        }
+});
+});

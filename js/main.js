39ba05b993874091b7a74c9b4363f16484e4fe35
Ahaewon0509 <cjstkthrkwhrgodqhrgo@naver.com>
2023-04-28 16:46:55 +0900

    v.4-3

--- js/main.js
+++ js/main.js
@@ -66,6 +66,7 @@ $(function(){
                 // 개별적으로 Wheel 이벤트 적용
                 $(this).on("mousewheel DOMMouseScroll", function (e) {
                     e.preventDefault();
+                    var nextindex = index;
                     var delta = 0;
                     if (!event) event = window.event;
                     if (event.wheelDelta) {
@@ -79,6 +80,7 @@ $(function(){
                     // 마우스휠을 위에서 아래로
                     if (delta < 0) {
                         if ($(elmSelecter).next() != undefined) {
+                            nextindex++;
                             try{
                                 moveTop = $(elmSelecter).next().offset().top;
                             }catch(e){}
@@ -86,16 +88,18 @@ $(function(){
                     // 마우스휠을 아래에서 위로
                     } else {
                         if ($(elmSelecter).prev() != undefined) {
-                            try{
-                                moveTop = $(elmSelecter).prev().offset().top;
-                            }catch(e){}
+                            nextindex--;
+                        try{
+                            moveTop = $(elmSelecter).prev().offset().top;
+                        }catch(e){}
                         }
                     }
                     // 화면 이동 0.8초(800)
                     $("html,body").stop().animate({
                         scrollTop: moveTop + 'px'
                     }, {
-                        duration: 800, complete: function () {
+                        duration: 700, complete: function () {
+                            $("#nav > a").eq(nextindex).addClass("active").siblings().removeClass("active");
                         }
                     });
                 });
